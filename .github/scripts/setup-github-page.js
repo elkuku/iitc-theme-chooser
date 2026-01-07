b69897a13ea3eddd094181a4896b7d176d6eab4a
elkuku <der.el.kuku@gmail.com>
2026-01-07 10:07:08 -0500

    Move the github stuff around

--- .github/scripts/setup-github-page.js
+++ .github/scripts/setup-github-page.js
@@ -12,7 +12,7 @@ const formattedDate = `${day}-${month}-${year}`
 fs.rmSync('gh_page', {recursive: true, force: true})
 fs.mkdirSync('gh_page', {recursive: true})
 
-fs.cpSync('github_page', 'gh_page', {recursive: true})
+fs.cpSync('.github/page', 'gh_page', {recursive: true})
 fs.cpSync('dist', 'gh_page/files', {recursive: true})
 
 const fileNames = fs
@@ -21,6 +21,7 @@ const fileNames = fs
     .map(entry => entry.name)
 
 const metaFile = fileNames.filter(fileName => fileName.endsWith('.meta.js'))[0]
+console.log('metaFile', metaFile)
 let version = 'n/a'
 if (metaFile) {
     const meta = fs.readFileSync(`dist/${metaFile}`, 'utf8')
@@ -42,10 +43,10 @@ let template = fs.readFileSync('gh_page/index.html', 'utf8')
 const projectName = pluginData.name.replace('IITC plugin: ', '')
 
 template = template.replace('{{FILE_LINKS}}', links)
-template = template.replaceAll('{{PROJECT_NAME}}', projectName)
-template = template.replaceAll('{{PROJECT_VERSION}}', version)
-template = template.replaceAll('{{LAST_UPDATED}}', formattedDate)
-template = template.replace('{{PROJECT_DESCRIPTION}}', pluginData.description)
+    .replaceAll('{{PROJECT_NAME}}', projectName)
+    .replaceAll('{{PROJECT_VERSION}}', version)
+    .replaceAll('{{LAST_UPDATED}}', formattedDate)
+    .replace('{{PROJECT_DESCRIPTION}}', pluginData.description)
 
 fs.writeFileSync('gh_page/index.html', template, 'utf8')
 
