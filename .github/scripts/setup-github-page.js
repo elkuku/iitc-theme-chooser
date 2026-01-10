46f41f84140ccd1ea549d921074ab65aa7c66ab2
elkuku <der.el.kuku@gmail.com>
2026-01-10 10:30:36 -0500

    Update workflow

--- .github/scripts/setup-github-page.js
+++ .github/scripts/setup-github-page.js
@@ -1,4 +1,13 @@
+#!/usr/bin/env node
+
 import fs from 'fs'
+
+function escapeHtml(value = '') {
+    return value
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;');
+}
 
 console.log('Generating GitHub page...')
 
@@ -13,18 +22,28 @@ fs.rmSync('gh_page', {recursive: true, force: true})
 fs.mkdirSync('gh_page', {recursive: true})
 
 fs.cpSync('.github/page', 'gh_page', {recursive: true})
-fs.cpSync('dist', 'gh_page/files', {recursive: true})
+fs.cpSync('build', 'gh_page/files', {recursive: true})
 
-const fileNames = fs
-    .readdirSync('dist', {withFileTypes: true})
-    .filter(entry => entry.isFile())
-    .map(entry => entry.name)
+let releaseFiles = [], devFiles = []
 
-const metaFile = fileNames.filter(fileName => fileName.endsWith('.meta.js'))[0]
-console.log('metaFile', metaFile)
+if (fs.existsSync('build/release')) {
+    releaseFiles = fs
+        .readdirSync('build/release', {withFileTypes: true})
+        .filter(entry => entry.isFile())
+        .map(entry => entry.name)
+}
+
+if (fs.existsSync('build/dev')) {
+    devFiles = fs
+        .readdirSync('build/dev', {withFileTypes: true})
+        .filter(entry => entry.isFile())
+        .map(entry => entry.name)
+}
+
+const metaFile = releaseFiles.filter(fileName => fileName.endsWith('.meta.js'))[0]
 let version = 'n/a'
 if (metaFile) {
-    const meta = fs.readFileSync(`dist/${metaFile}`, 'utf8')
+    const meta = fs.readFileSync(`build/release/${metaFile}`, 'utf8')
     const match = meta.match(/^\s*\/\/\s*@version\s+(.+)$/m)
     version = match ? match[1].trim() : 'n/a'
 }
@@ -33,20 +52,42 @@ const pluginData = JSON.parse(
     fs.readFileSync('plugin.json', 'utf8'),
 )
 
-const links = fileNames
+let releaseLinks = []
+if (releaseFiles.length > 0) {
+    releaseLinks = releaseFiles
+        .filter(name => !name.endsWith('meta.js'))
+        .map(name => `<li><a href="files/release/${name}">${name}</a></li>`)
+        .join('\n')
+
+} else {
+    releaseLinks = '<li>No release yet</li>'
+}
+
+const devLinks = devFiles
     .filter(name => !name.endsWith('meta.js'))
-    .map(name => `<li><a href="files/${name}">${name}</a></li>`)
+    .map(name => `<li><a href="files/dev/${name}">${name}</a></li>`)
     .join('\n')
 
 let template = fs.readFileSync('gh_page/index.html', 'utf8')
 
 const projectName = pluginData.name.replace('IITC plugin: ', '')
 
-template = template.replace('{{FILE_LINKS}}', links)
+const raw = fs.readFileSync('build/changelog.json', 'utf8')
+const tags = JSON.parse(raw);
+const changelog = tags.map(tag => `
+      <tr>
+        <td>${escapeHtml(tag.name)}</td>
+        <td><pre class="changelog">${escapeHtml(tag.message)}</pre></td>
+      </tr>
+  `).join('')
+
+template = template.replace('{{DEV_LINKS}}', devLinks)
+    .replace('{{RELEASE_LINKS}}', releaseLinks)
     .replaceAll('{{PROJECT_NAME}}', projectName)
     .replaceAll('{{PROJECT_VERSION}}', version)
     .replaceAll('{{LAST_UPDATED}}', formattedDate)
     .replace('{{PROJECT_DESCRIPTION}}', pluginData.description)
+    .replace('{{CHANGELOG}}', changelog)
 
 fs.writeFileSync('gh_page/index.html', template, 'utf8')
 
